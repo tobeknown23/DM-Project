f7bc81b1c760c716af9082de992539a0980f4378
TOBEKNOWNABBAS <61627416+TOBEKNOWNABBAS@users.noreply.github.com>
2020-05-19 01:01:17 +0500

    Add files via upload
    here is a cipher code generated for a sample image of mine we have completed the class of RSA Algorithm and also we have done encryption part of the project

---
+++ RSA.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageCryptography
+{
+    class RSA
+    {
+        public static long sqr(long sq)
+        {
+            return (sq *sq);
+        }
+
+        public static long mod(int p, int q, int r) 
+        {
+            if (p == 0)
+                return 1;
+            else if (p % 2 == 0)
+                return sqr(mod(p, q / 2, r)) % r;
+            else
+                return ((p % r) * mod(p, q - 1, r)) % r;
+        }
+
+        public static int mul_of_prime(int p1, int p2)
+        {
+            return (p1 * p2);
+        }
+
+        public static int cal_phi(int p1, int p2)
+        {
+            return ((p1 - 1) * (p2 - 1));
+        }
+
+        public static Int32 privateKey(int a, int b, int c)
+        {
+            int x = 0;
+            int RES = 0;
+
+            for (c = 1; ; c++)
+            {
+                RES = (c * b) % a;
+                if (RES == 1) break;
+            }
+            return c;
+        }
+
+
+    }
+}
+}
